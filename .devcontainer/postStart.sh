b88786183702e9a63e8306c97e245ad431deb216
k5-mot <34744243+k5-mot@users.noreply.github.com>
2025-05-21 00:29:06 +0900

    A

---
+++ .devcontainer/postStart.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+printf "\e[36mpostStartCommand\e[0m\n"
+
+printf "\e[34mSetup Backend environment...\e[0m\n"
+pushd backend
+uv python install 3.13
+# uv sync --dev
+popd
+
+printf "\e[34mSetup Frontend environment...\e[0m\n"
+pushd frontend
+# volta install node@22
+# volta install pnpm git-cz
+# npm install -g npm pnpm git-cz
+# npm install
+# rm -rfv node_modules package-lock.json .next
+# npm cache clean --force
+npm -g install npm git-cz
+npm install
+popd
