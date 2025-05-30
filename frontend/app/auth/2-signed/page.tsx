dc4c44e0f9f9c6d763fe1f0c79704d507fc02ee6
k5-mot <34744243+k5-mot@users.noreply.github.com>
2025-05-30 13:34:26 +0000

    A

--- frontend/app/auth/2-signed/page.tsx
+++ frontend/app/auth/2-signed/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Typography, Container, CircularProgress } from "@mui/material";
 import { useEffect } from "react";
-// import congraturations from "@/public/congratulations.png";
 import Image from "next/image";
 
 export default function Signed() {
